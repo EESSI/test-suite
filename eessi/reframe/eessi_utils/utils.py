45531b255ac42bec6e8409b288613e2e5bc03756
Samuel Moors <samuel.moors@vub.be>
2022-12-12 15:10:45 +0100

    fix code style for eessi_utils/utils.py

--- eessi/reframe/eessi_utils/utils.py
+++ eessi/reframe/eessi_utils/utils.py
@@ -2,11 +2,11 @@ import re
 
 import reframe as rfm
 
-gpu_dev_name = 'gpu'
+GPU_DEV_NAME = 'gpu'
 
 
 def _get_gpu_list(test: rfm.RegressionTest):
-    return [ dev.num_devices for dev in test.current_partition.devices if dev.device_type == gpu_dev_name ]
+    return [dev.num_devices for dev in test.current_partition.devices if dev.device_type == GPU_DEV_NAME]
 
 
 def get_num_gpus(test: rfm.RegressionTest) -> int:
@@ -14,9 +14,9 @@ def get_num_gpus(test: rfm.RegressionTest) -> int:
     gpu_list = _get_gpu_list(test)
     # If multiple devices are called 'GPU' in the current partition,
     # we don't know for which to return the device count...
-    if(len(gpu_list) != 1):
-        raise ValueError(f"Multiple different devices exist with the name " 
-                         f"'{gpu_dev_name}' for partition '{test.current_partition.name}'. "
+    if len(gpu_list) != 1:
+        raise ValueError(f"Multiple different devices exist with the name "
+                         f"'{GPU_DEV_NAME}' for partition '{test.current_partition.name}'. "
                          f"Cannot determine number of GPUs available for the test. "
                          f"Please check the definition of partition '{test.current_partition.name}' "
                          f"in your ReFrame config file.")
@@ -26,7 +26,7 @@ def get_num_gpus(test: rfm.RegressionTest) -> int:
 
 def is_gpu_present(test: rfm.RegressionTest) -> bool:
     '''Checks if GPUs are present in the current partition'''
-    return ( len(_get_gpu_list(test)) >= 1 )
+    return len(_get_gpu_list(test)) >= 1
 
 
 def is_cuda_required_module(module_name):
