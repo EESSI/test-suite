3835a8730e21e0fa5a9c2c5c3651ab339a060687
Samuel Moors <samuel.moors@vub.be>
2023-02-28 12:17:39 +0100

    scale number of cpus per node with number of GPUs requested

--- eessi/reframe/eessi_utils/hooks.py
+++ eessi/reframe/eessi_utils/hooks.py
@@ -45,31 +45,39 @@ def assign_one_task_per_cpu(test: rfm.RegressionTest, num_nodes: int) -> rfm.Reg
 
 def assign_one_task_per_gpu(test: rfm.RegressionTest, num_nodes: int) -> rfm.RegressionTest:
     '''
-    Sets num_tasks_per_node to the number of gpus,
-    and num_cpus_per_task to the number of CPUs per node available per GPU in this partition,
-    unless specified with --setvar num_tasks_per_node=<x> and/or --setvar num_cpus_per_task=<y>
-    Also sets num_gpus_per_node unless specified with --setvar num_gpus_per_node=<z>:
-    - if num_tasks_per_node is not set, set num_gpus_per_node equal to nb of GPUs per node available in this partition
+    Sets num_tasks_per_node, num_cpus_per_task, and num_gpus_per_node,
+        unless specified with
+            --setvar num_tasks_per_node=<x> and/or
+            --setvar num_cpus_per_task=<y> and/or
+            --setvar num_gpus_per_node=<z>
+    - default num_gpus_per_node = total nb of GPUs per node available in this partition
+    - default num_tasks_per_node = num_gpus_per_node
+    - default num_cpus_per_task = total nb of CPUs per GPU available in this partition, divided by num_tasks_per_node
     - if num_tasks_per_node is set, set num_gpus_per_node equal to either num_tasks_per_node or nb of GPUs per node
         available in this partition (whatever is smallest).
     '''
-    if test.current_partition.processor.num_cpus is None:
+    num_cpus = test.current_partition.processor.num_cpus
+    if num_cpus is None:
         raise AttributeError(processor_info_missing)
 
+    num_tasks_per_node = test.num_tasks_per_node
+    num_gpus_per_node = test.num_gpus_per_node
     max_gpus_per_node = utils.get_num_gpus_per_node(test)
 
-    if not test.num_tasks_per_node:
-        if not test.num_gpus_per_node:
-            test.num_gpus_per_node = max_gpus_per_node
-        test.num_tasks_per_node = test.num_gpus_per_node
+    if not num_tasks_per_node:
+        if not num_gpus_per_node:
+            num_gpus_per_node = max_gpus_per_node
+        num_tasks_per_node = num_gpus_per_node
 
-    elif not test.num_gpus_per_node:
-        test.num_gpus_per_node = min(test.num_tasks_per_node, max_gpus_per_node)
+    elif not num_gpus_per_node:
+        num_gpus_per_node = min(num_tasks_per_node, max_gpus_per_node)
 
     if not test.num_cpus_per_task:
-        test.num_cpus_per_task = int(test.current_partition.processor.num_cpus / test.num_tasks_per_node)
+        test.num_cpus_per_task = int((num_cpus * num_gpus_per_node) / (num_tasks_per_node * max_gpus_per_node))
 
-    test.num_tasks = num_nodes * test.num_tasks_per_node
+    test.num_gpus_per_node = num_gpus_per_node
+    test.num_tasks_per_node = num_tasks_per_node
+    test.num_tasks = num_nodes * num_tasks_per_node
 
 
 def auto_assign_num_tasks_MPI(test: rfm.RegressionTest, num_nodes: int) -> rfm.RegressionTest:
