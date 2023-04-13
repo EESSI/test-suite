e1ae3cbcf04cb18a63e3f424e7338c44acdaa582
Samuel Moors <samuel.moors@vub.be>
2023-04-13 09:40:43 +0200

    align docstring indentations

--- eessi/reframe/eessi_utils/hooks.py
+++ eessi/reframe/eessi_utils/hooks.py
@@ -5,19 +5,20 @@ Hooks for adding tags, filtering and setting job resources in ReFrame tests
 import reframe as rfm
 from eessi_utils import utils
 
-PROCESSOR_INFO_MISSING = '''This test requires the number of CPUs to be known for the partition it runs on.
+PROCESSOR_INFO_MISSING = '''
+This test requires the number of CPUs to be known for the partition it runs on.
 Check that processor information is either autodetected
-    (see https://reframe-hpc.readthedocs.io/en/stable/configure.html#proc-autodetection),
-    or manually set in the ReFrame configuration file
-    (see https://reframe-hpc.readthedocs.io/en/stable/config_reference.html#processor-info).
+(see https://reframe-hpc.readthedocs.io/en/stable/configure.html#proc-autodetection),
+or manually set in the ReFrame configuration file
+(see https://reframe-hpc.readthedocs.io/en/stable/config_reference.html#processor-info).
 '''
 
 
 def assign_one_task_per_compute_unit(test: rfm.RegressionTest, compute_unit: str) -> rfm.RegressionTest:
     """
-    Assign one task per compute unit ('gpu' or 'cpu')
-    Automatically sets num_tasks, num_tasks_per_node, num_cpus_per_task, and num_gpus_per_node based on the current
-        partition's num_cpus, num_gpus_per_node and test.num_nodes.
+    Assign one task per compute unit ('gpu' or 'cpu').
+    Automatically sets num_tasks, num_tasks_per_node, num_cpus_per_task, and num_gpus_per_node,
+    based on the current partition's num_cpus, num_gpus_per_node and test.num_nodes.
     For GPU tests, one task per GPU is set, and num_cpus_per_task is based on the ratio of CPU-cores/GPUs.
     For CPU tests, one task per CPU is set, and num_cpus_per_task is set to 1.
     Total task count is determined based on the number of nodes to be used in the test.
@@ -39,7 +40,7 @@ def assign_one_task_per_compute_unit(test: rfm.RegressionTest, compute_unit: str
 def assign_one_task_per_cpu(test: rfm.RegressionTest) -> rfm.RegressionTest:
     """
     Sets num_tasks_per_node and num_cpus_per_task such that it will run one task per core,
-        unless specified with --setvar num_tasks_per_node=<x> and/or --setvar num_cpus_per_task=<y>
+    unless specified with --setvar num_tasks_per_node=<x> and/or --setvar num_cpus_per_task=<y>.
     """
     max_cpus_per_node = test.max_cpus_per_node
     num_tasks_per_node = test.num_tasks_per_node
@@ -61,16 +62,18 @@ def assign_one_task_per_cpu(test: rfm.RegressionTest) -> rfm.RegressionTest:
 
 def assign_one_task_per_gpu(test: rfm.RegressionTest) -> rfm.RegressionTest:
     """
-    Sets num_tasks_per_node, num_cpus_per_task, and num_gpus_per_node,
-        unless specified with
-            --setvar num_tasks_per_node=<x> and/or
-            --setvar num_cpus_per_task=<y> and/or
-            --setvar num_gpus_per_node=<z>
-    - default num_gpus_per_node = total nb of GPUs per node available in this partition
-    - default num_tasks_per_node = num_gpus_per_node
-    - default num_cpus_per_task = total nb of CPUs per GPU available in this partition, divided by num_tasks_per_node
-    - if num_tasks_per_node is set, set num_gpus_per_node equal to either num_tasks_per_node or nb of GPUs per node
-        available in this partition (whatever is smallest).
+    Sets num_tasks_per_node, num_cpus_per_task, and num_gpus_per_node, unless specified with:
+    --setvar num_tasks_per_node=<x> and/or
+    --setvar num_cpus_per_task=<y> and/or
+    --setvar num_gpus_per_node=<z>.
+
+    Default values:
+    num_gpus_per_node = total nb of GPUs per node available in this partition
+    num_tasks_per_node = num_gpus_per_node
+    num_cpus_per_task = total nb of CPUs per GPU available in this partition, divided by num_tasks_per_node
+
+    If num_tasks_per_node is set, set num_gpus_per_node equal to either num_tasks_per_node,
+    or nb of GPUs per node available in this partition (whatever is smallest).
     """
     max_gpus_per_node = utils.get_num_gpus_per_node(test)
     max_cpus_per_node = test.max_cpus_per_node
@@ -97,8 +100,8 @@ def assign_one_task_per_gpu(test: rfm.RegressionTest) -> rfm.RegressionTest:
 
 def filter_tests_by_device_type(test: rfm.RegressionTest, required_device_type: str):
     """
-    filter tests by required device type and whether the module supports CUDA,
-        unless valid_systems is specified with --setvar valid_systems=<comma-separated-list>
+    Filter tests by required device type and whether the module supports CUDA,
+    unless valid_systems is specified with --setvar valid_systems=<comma-separated-list>.
     """
     if not test.valid_systems:
         is_cuda_module = utils.is_cuda_required_module(test.module_name)
@@ -123,8 +126,8 @@ def filter_tests_by_device_type(test: rfm.RegressionTest, required_device_type: 
 
 def set_modules(test: rfm.RegressionTest):
     """
-    skip this test if module_name is not among a list of modules,
-        specified with --setvar modules=<comma-separated-list>
+    Skip this test if module_name is not among a list of modules,
+    specified with --setvar modules=<comma-separated-list>.
     """
     if test.modules and test.module_name not in test.modules:
         test.valid_systems = []
