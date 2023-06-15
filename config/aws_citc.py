cffe2710278612345bb0743287130a9b23bcbc10
casparvl <casparvl@mgmt.fair-mastodon.citc.local>
2023-06-15 11:59:13 +0000

    Make sure vars don't get exported, we need to source the EESSI environment on the batch node to get the right architecture prefix

--- config/aws_citc.py
+++ config/aws_citc.py
@@ -10,77 +10,77 @@ site_configuration = {
             'partitions': [
                 {
                     'name': 'c4.2xlarge-haswell',
-                    'access': ['--constraint shape=c4.2xlarge'],
+                    'access': ['--constraint shape=c4.2xlarge', '--export=NONE'],
                     'descr': 'Haswell, 8 cores, 15 GiB',
                 },
                 {
                     'name': 'c4.4xlarge-haswell',
-                    'access': ['--constraint shape=c4.4xlarge'],
+                    'access': ['--constraint shape=c4.4xlarge', '--export=NONE'],
                     'descr': 'Haswell, 16 cores, 30 GiB',
                 },
                 {
                     'name': 'c5a.2xlarge-zen2',
-                    'access': ['--constraint shape=c5a.2xlarge'],
+                    'access': ['--constraint shape=c5a.2xlarge', '--export=NONE'],
                     'descr': 'Zen2, 8 cores, 16 GiB',
                 },
                 {
                     'name': 'c5a.4xlarge-zen2',
-                    'access': ['--constraint shape=c5a.4xlarge'],
+                    'access': ['--constraint shape=c5a.4xlarge', '--export=NONE'],
                     'descr': 'Zen2, 16 cores, 32 GiB',
                 },
                 {
                     'name': 'c6a.2xlarge-zen3',
-                    'access': ['--constraint shape=c6a.2xlarge'],
+                    'access': ['--constraint shape=c6a.2xlarge', '--export=NONE'],
                     'descr': 'Zen3, 8 cores, 16 GiB',
                 },
                 {
                     'name': 'c6a.4xlarge-zen3',
-                    'access': ['--constraint shape=c6a.4xlarge'],
+                    'access': ['--constraint shape=c6a.4xlarge', '--export=NONE'],
                     'descr': 'Zen3, 16 cores, 32 GiB',
                 },
                 {
                     'name': 'c5.2xlarge-skylake-cascadelake',
-                    'access': ['--constraint shape=c5.2xlarge'],
+                    'access': ['--constraint shape=c5.2xlarge', '--export=NONE'],
                     'descr': 'Skylake/Cascade lake, 8 cores, 16 GiB',
                 },
                 {
                     'name': 'c5.4xlarge-skylake-cascadelake',
-                    'access': ['--constraint shape=c5.4xlarge'],
+                    'access': ['--constraint shape=c5.4xlarge', '--export=NONE'],
                     'descr': 'Skylake/Cascade lake, 16 cores, 32 GiB',
                 },
                 {
                     'name': 'c5d.2xlarge-skylake-cascadelake',
-                    'access': ['--constraint shape=c5d.2xlarge'],
+                    'access': ['--constraint shape=c5d.2xlarge', '--export=NONE'],
                     'descr': 'Skylake/Cascade lake, 8 cores, 16 GiB, 200GB NVMe',
                 },
                 {
                     'name': 'c6i.2xlarge-icelake',
-                    'access': ['--constraint shape=c6i.2xlarge'],
+                    'access': ['--constraint shape=c6i.2xlarge', '--export=NONE'],
                     'descr': 'Icelake, 8 cores, 16 GiB',
                 },
                 {
                     'name': 'c6g.2xlarge-graviton2',
-                    'access': ['--constraint shape=c6g.2xlarge'],
+                    'access': ['--constraint shape=c6g.2xlarge', '--export=NONE'],
                     'descr': 'Graviton2, 8 cores, 16 GiB',
                 },
                 {
-                    'name': 'c6g.4xlarge-graviton2)',
-                    'access': ['--constraint shape=c6g.4xlarge'],
+                    'name': 'c6g.4xlarge-graviton2',
+                    'access': ['--constraint shape=c6g.4xlarge', '--export=NONE'],
                     'descr': 'Graviton2, 16 cores, 32 GiB',
                 },
                 {
-                    'name': 'c6g.8xlarge-graviton2)',
-                    'access': ['--constraint shape=c6g.8xlarge'],
+                    'name': 'c6g.8xlarge-graviton2',
+                    'access': ['--constraint shape=c6g.8xlarge', '--export=NONE'],
                     'descr': 'Graviton2, 32 cores, 64 GiB',
                 },
                 {
-                    'name': 'c7g.2xlarge-graviton3)',
-                    'access': ['--constraint shape=c7g.2xlarge'],
+                    'name': 'c7g.2xlarge-graviton3',
+                    'access': ['--constraint shape=c7g.2xlarge', '--export=NONE'],
                     'descr': 'Graviton3, 8 cores, 16 GiB',
                 },
                 {
-                    'name': 'c7g.4xlarge-graviton3)',
-                    'access': ['--constraint shape=c7g.4xlarge'],
+                    'name': 'c7g.4xlarge-graviton3',
+                    'access': ['--constraint shape=c7g.4xlarge', '--export=NONE'],
                     'descr': 'Graviton3, 16 cores, 32 GiB',
                 },
              ]
@@ -143,10 +143,10 @@ partition_defaults = {
     'scheduler': 'squeue',
     # Can't use mpirun, because there is no system mpirun on citc
     # See https://github.com/EESSI/test-suite/pull/53#issuecomment-1590849226
-    'launcher': 'srun',
+    'launcher': 'mpirun',
     'environs': ['default'],
     'features': ['cpu'],
-    'prepare_cmds': ['source /cvmfs/pilot.eessi-hpc.org/latest/init/bash'],
+    'prepare_cmds': ['source /cvmfs/pilot.eessi-hpc.org/latest/init/bash '],
 }
 for system in site_configuration['systems']:
     for partition in system['partitions']:
