d134a98db9202da7f1020935b6cb6ec2bb87b213
Samuel Moors <samuel.moors@vub.be>
2023-10-19 09:36:14 +0200

    remove aliases

--- eessi/testsuite/constants.py
+++ eessi/testsuite/constants.py
@@ -46,8 +46,8 @@ SCALES = {
     '1_core': {'num_nodes': 1, 'num_cpus_per_node': 1, 'num_gpus_per_node': 1},
     '2_cores': {'num_nodes': 1, 'num_cpus_per_node': 2, 'num_gpus_per_node': 1},
     '4_cores': {'num_nodes': 1, 'num_cpus_per_node': 4, 'num_gpus_per_node': 1},
-    '1_core_per_node_2_nodes': {'num_nodes': 2, 'num_cpus_per_node': 1, 'num_gpus_per_node': 1},
-    '1_core_per_node_4_nodes': {'num_nodes': 4, 'num_cpus_per_node': 1, 'num_gpus_per_node': 1},
+    '1_cpn_2_nodes': {'num_nodes': 2, 'num_cpus_per_node': 1, 'num_gpus_per_node': 1},
+    '1_cpn_4_nodes': {'num_nodes': 4, 'num_cpus_per_node': 1, 'num_gpus_per_node': 1},
     '1_8_node': {'num_nodes': 1, 'node_part': 8},  # 1/8 node
     '1_4_node': {'num_nodes': 1, 'node_part': 4},  # 1/4 node
     '1_2_node': {'num_nodes': 1, 'node_part': 2},  # 1/2 node
@@ -57,7 +57,3 @@ SCALES = {
     '8_nodes': {'num_nodes': 8, 'node_part': 1},
     '16_nodes': {'num_nodes': 16, 'node_part': 1},
 }
-
-# aliases
-SCALES['1_cpn_2_nodes'] = SCALES['1_core_per_node_2_nodes']
-SCALES['1_cpn_4_nodes'] = SCALES['1_core_per_node_4_nodes']
