0cea03fea8ec81ac60fe85bc068a439340d67030
Caspar van Leeuwen <casparl@int6.local.snellius.surf.nl>
2024-03-12 21:44:37 +0100

    software.eessi.io is now also present on the AWS MC cluster

--- CI/aws_mc/ci_config.sh
+++ CI/aws_mc/ci_config.sh
@@ -2,7 +2,3 @@
 if [ -z "${REFRAME_ARGS}" ]; then
    REFRAME_ARGS="--tag CI --tag 1_node|2_nodes"
 fi
-# For now, software.eessi.io is not yet deployed on login nodes of the AWS MC cluster
-if [ -z "${EESSI_CVMFS_REPO}" ]; then
-   EESSI_CVMFS_REPO="/cvmfs/pilot.eessi-hpc.org"
-fi
