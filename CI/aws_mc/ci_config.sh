d36a0de914a1252ea01d4bb07aa8b84ae5af535b
Caspar van Leeuwen <casparl@int6.local.snellius.surf.nl>
2024-03-12 15:37:39 +0100

    Make exception for AWS_MC cluster and run pilot, since software.eessi.io is not deployed on the login nodes there

--- CI/aws_mc/ci_config.sh
+++ CI/aws_mc/ci_config.sh
@@ -1,2 +1,4 @@
 # Configurable items
 REFRAME_ARGS="--tag CI --tag 1_node|2_nodes"
+# For now, software.eessi.io is not yet deployed on login nodes of the AWS MC cluster
+EESSI_CVMFS_REPO="/cvmfs/pilot.eessi-hpc.org"
