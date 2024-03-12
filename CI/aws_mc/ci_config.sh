71a7e0bfbc2cf2a741649f097c6e7a05438884c6
Caspar van Leeuwen <casparl@int6.local.snellius.surf.nl>
2024-03-12 16:10:39 +0100

    Make sure things set in the environment take priority over the ci_config.sh settings

--- CI/aws_mc/ci_config.sh
+++ CI/aws_mc/ci_config.sh
@@ -1,4 +1,8 @@
 # Configurable items
-REFRAME_ARGS="--tag CI --tag 1_node|2_nodes"
+if [ -z "${REFRAME_ARGS}" ]; then
+   REFRAME_ARGS="--tag CI --tag 1_node|2_nodes"
+fi
 # For now, software.eessi.io is not yet deployed on login nodes of the AWS MC cluster
-EESSI_CVMFS_REPO="/cvmfs/pilot.eessi-hpc.org"
+if [ -z "${EESSI_CVMFS_REPO}" ]; then
+   EESSI_CVMFS_REPO="/cvmfs/pilot.eessi-hpc.org"
+fi
