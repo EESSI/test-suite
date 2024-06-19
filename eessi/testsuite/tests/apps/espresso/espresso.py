e1126ebc3f6e74a8cb0241c1fd37bc7bbf7bdca8
Satish Kamath <satishk@tcn3.local.snellius.surf.nl>
2024-06-19 21:14:44 +0200

    Trying to make the linter happy.

--- eessi/testsuite/tests/apps/espresso/espresso.py
+++ eessi/testsuite/tests/apps/espresso/espresso.py
@@ -69,7 +69,7 @@ class EESSI_ESPRESSO(rfm.RunOnlyRegressionTest):
     @run_after('init')
     def set_tag_ci(self):
         """ Setting tests under CI tag. """
-        if (self.benchmark_info[0] in ['mpi.ionic_crystals.p3m', 'mpi.particles.lj'] and \
+        if (self.benchmark_info[0] in ['mpi.ionic_crystals.p3m', 'mpi.particles.lj'] and 
                 SCALES[self.scale]['num_nodes'] < 2):
             self.tags.add('CI')
             log(f'tags set to {self.tags}')
