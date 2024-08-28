ec811fa1864882ee526179eb37b88e7024e5594b
Caspar van Leeuwen <casparvanl@hotmail.com>
2024-08-28 16:16:22 +0200

    Also for hortense, rely on SBATCH_ACCOUNT instead of having that hardcoded in the config

--- config/vsc_hortense.py
+++ config/vsc_hortense.py
@@ -13,7 +13,8 @@ from eessi.testsuite.constants import *  # noqa: F403
 
 account = "my-slurm-account"
 
-hortense_access = [f'-A {account}', '--export=NONE', '--get-user-env=60L']
+# Note that we rely on the SBATCH_ACCOUNT environment variable to be specified
+hortense_access = [f'--export=NONE', '--get-user-env=60L']
 
 
 @register_launcher('mympirun')
