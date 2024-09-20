5108a85d9cb16efb16b7aaa0c5923f9fc6740374
Caspar van Leeuwen <casparl@int4.local.snellius.surf.nl>
2024-09-20 23:09:27 +0200

    Make things consistent..

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ import setuptools
 
 setuptools.setup(
     use_scm_version={'write_to': 'eessi/testsuite/_version.py'},
-    setup_requires=['setuptools_scm<7','packaging<21.3'],
+    setup_requires=['setuptools_scm<7','packaging<=21.3'],
 )
