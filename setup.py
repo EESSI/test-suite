84178cd6053e9524babf896a8c6e4b5cc17eceaf
Caspar van Leeuwen <casparl@int4.local.snellius.surf.nl>
2024-09-20 22:45:09 +0200

    Make things conditional on version of setuptools_scm directly

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
-import sys
 import setuptools
+import pkg_resources
 
 # write_to got replaced by version_file starting from setuptools_scm v8.
-# This version is only supported for python 3.8 and above
+def get_setuptools_scm_version():
+    try:
+        scm_version = pkg_resources.get_distribution("setuptools_scm").version
+        return tuple(map(int, scm_version.split(".")[:2]))  # Convert version string to tuple, e.g., (8, 0)
+    except pkg_resources.DistributionNotFound:
+        return (0, 0)  # If setuptools_scm is not found, assume version 0.0
+
+scm_version = get_setuptools_scm_version()
+
 python_version = sys.version_info
-if python_version < (3, 8):
-    scm_arg_key = "write_to"
-else:
+if scm_version >= (8, 0):
     scm_arg_key = "version_file"
+else:
+    scm_arg_key = "write_to"
 
 setuptools.setup(
     use_scm_version={scm_arg_key: "eessi/testsuite/_version.py"},
