c8bf4a674b1d46a1d3cdd4a003d3f30209f80767
Caspar van Leeuwen <casparl@int4.local.snellius.surf.nl>
2024-09-20 23:29:24 +0200

    let setup.py handle the setuptools_scm conditionally, see if that works...

--- setup.py
+++ setup.py
@@ -1,6 +1,18 @@
 import setuptools
+import sys
+
+scm_version = get_setuptools_scm_version()
+
+python_version = sys.version_info
+if python_version < (3, 8):
+    scm_require='setuptools_scm<7'
+    scm_arg_key = "write_to"
+else:
+    scm_require='setuptools_scm>=8'
+    scm_arg_key = "version_file"
+
 
 setuptools.setup(
-    use_scm_version={'write_to': 'eessi/testsuite/_version.py'},
-    setup_requires=['setuptools_scm<7','packaging<=21.3'],
+    use_scm_version={scm_arg_key: 'eessi/testsuite/_version.py'},
+    setup_requires=[scm_require],
 )
