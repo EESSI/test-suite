c82110649ea0df46c1c3143e379d299a254fb540
Caspar van Leeuwen <casparl@int4.local.snellius.surf.nl>
2024-09-20 22:32:45 +0200

    Make usage of write_to or version_file dependent on the version of python (and thus the version of setuptools_scm) used

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
+import sys
 import setuptools
 
+# write_to got replaced by version_file starting from setuptools_scm v8.
+# This version is only supported for python 3.8 and above
+python_version = sys.version_info
+if python_version < (3, 8):
+    scm_arg_key = "write_to"
+else:
+    scm_arg_key = "version_file"
+
 setuptools.setup(
-    use_scm_version={"version_file": "eessi/testsuite/_version.py"},
-    setup_requires=['setuptools_scm>=8'],
+    use_scm_version={scm_arg_key: "eessi/testsuite/_version.py"},
+    setup_requires=['setuptools_scm'],
 )
