38ffee1dd59db661cdc1f1ae223f5cd63191113f
Caspar van Leeuwen <casparl@int4.local.snellius.surf.nl>
2024-09-21 11:11:24 +0200

    Split off the setuptools_scm versioning into a different pr https://github.com/EESSI/test-suite/pull/185 . The current PR now depends on this, as it relies on the eessi/testsuite/__init__.py file to provide a version

--- setup.py
+++ setup.py
@@ -1,16 +1,2 @@
 import setuptools
-import sys
-
-python_version = sys.version_info
-if python_version < (3, 8):
-    scm_require = ['packaging<=21.3', 'setuptools_scm<7']
-    scm_arg_key = "write_to"
-else:
-    scm_require = ['setuptools_scm>=8']
-    scm_arg_key = "version_file"
-
-
-setuptools.setup(
-    use_scm_version={scm_arg_key: 'eessi/testsuite/_version.py'},
-    setup_requires=scm_require,
-)
+setuptools.setup()
