8c9bcf3dcbd0589b7b5bcfffcc5159a0999e9989
Caspar van Leeuwen <casparl@int4.local.snellius.surf.nl>
2024-09-23 16:53:44 +0200

    First, try if we can single source from setup.py for new enough setuptools_Scm

--- setup.py
+++ setup.py
@@ -3,16 +3,19 @@ import sys
 
 python_version = sys.version_info
 if python_version < (3, 8):
+    scm_dict = {'write_to': 'eessi/testsuite/_version.py'}
     scm_require = ['packaging<=21.3', 'setuptools_scm<7']
-    scm_arg_key = "write_to"
+#    scm_arg_key = "write_to"
 else:
+    scm_dict = {'version_file': 'eessi/testsuite/_version.py', 'fallback_version': '80.0.0'}
     scm_require = ['setuptools_scm>=8']
-    scm_arg_key = "version_file"
+#    scm_arg_key = "version_file"
 
-
-from eessi.testsuite import __version__
+# sys.path.append('.')
+# from eessi.testsuite import __version__
 
 setuptools.setup(
-    use_scm_version={scm_arg_key: 'eessi/testsuite/_version.py', 'fallback_version' = __version__},
+#    use_scm_version={scm_arg_key: 'eessi/testsuite/_version.py', 'fallback_version': 80.0.0},
+    use_scm_version=scm_dict,
     setup_requires=scm_require,
 )
