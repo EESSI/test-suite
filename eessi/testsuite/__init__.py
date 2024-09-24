a6afd1ae691f030860355f1951a962839364d863
Caspar van Leeuwen <casparl@int4.local.snellius.surf.nl>
2024-09-24 15:17:48 +0200

    Fix flake8

--- eessi/testsuite/__init__.py
+++ eessi/testsuite/__init__.py
@@ -19,23 +19,23 @@ except ImportError:
             # Using a relative path for relative_to doesn't work, because it will be relative to the current working
             # directory (which could be anywhere)
             # __file__ is the location of this init file (a full path), and this gives us a predictable path to the root
-            # (namely: two levels up)
-            # Note that if we ever move this __init__ file relative to the root of the git tree, we'll need to adjust this
+            # (namely: two levels up). Note that if we ever move this __init__ file relative to the root of the git
+            # tree, we'll need to adjust this
             __version__ = get_version(root='../..', relative_to=__file__)
         except (ImportError, LookupError):
             # If running from a tarball (e.g. release tarball) downloaded from github, we will not have the .git
             # folder available. Thus, setuptools_scm cannot determine the version in any way. Thus, use the
             # fallback_version from the pyproject.toml file (which doesn't exist when this is installed as a package,
             # but SHOULD exist when this is run from a downloaded tarball from git)
-    
+
             # Pyproject.toml should be two levels up from this file
             import os.path
             pyproject_toml = "%s/../../pyproject.toml" % os.path.dirname(__file__)
-    
+
             # Variables to track if we're in the right section and to store the fallback_version
             in_setuptools_scm_section = False
             fallback_version = None
-    
+
             file = None
             try:
                 file = open(pyproject_toml, 'r')
@@ -51,7 +51,7 @@ except ImportError:
                         elif stripped_line.startswith("[") and in_setuptools_scm_section:
                             # We've reached a new section, so stop searching
                             break
-    
+
                         # If we're in the right section, look for the fallback_version key
                         if in_setuptools_scm_section and stripped_line.startswith("fallback_version"):
                             # Extract the value after the '=' sign and strip any surrounding quotes or whitespace
