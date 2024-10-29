0eae61baea493b96ce36017f3bf345953fd44a3f
Caspar van Leeuwen <casparl@int5.local.snellius.surf.nl>
2024-10-29 10:54:30 +0100

    Accept any hostname for Snellius

--- config/surf_snellius.py
+++ config/surf_snellius.py
@@ -32,7 +32,9 @@ site_configuration = {
             'name': 'snellius',
             'descr': 'Dutch National Supercomputer',
             'modules_system': 'lmod',
-            'hostnames': ['int*', 'tcn*', 'hcn*', 'fcn*', 'gcn*', 'srv*'],
+            # Just accept any hostname. List of hostnames can only get outdated, and this is a single-system config
+            # file anyway.
+            'hostnames': ['*'],
             'prefix': reframe_prefix,
             'stagedir': f'/scratch-shared/{os.environ.get("USER")}/reframe_output/staging',
             'partitions': [
