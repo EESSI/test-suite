765399609955936fef9e7a11f86b4692384180b3
Caspar van Leeuwen <casparl@int6.local.snellius.surf.nl>
2026-05-05 18:43:44 +0200

    Put a try-guard around this import for newer pythons

--- eessi/__init__.py
+++ eessi/__init__.py
@@ -1,1 +1,9 @@
-__import__("pkg_resources").declare_namespace(__name__)
+# On older python's this is needed for correct installation of a namespace package
+# But on newer pythons, there is no pkg_resources anymore
+# We pragmatically wrap it in a try-block. It can be removed entirely after we stop supporting
+# python 3.6, since then we can simply require a new enough setuptools that this declare_namespace is
+# no longer required
+try:
+    __import__("pkg_resources").declare_namespace(__name__)
+except:
+    pass
