0dc7792a8f2f21e5ba9180b986c3c1b0e4bc92c2
Sam Moors <smoors@users.noreply.github.com>
2026-05-05 16:59:07 +0200

    fix typo

--- eessi/testsuite/tests/apps/MetalWalls.py
+++ eessi/testsuite/tests/apps/MetalWalls.py
@@ -67,7 +67,7 @@ class EESSI_MetalWalls_MW(MetalWallsCheck, EESSI_Mixin):
             self.compute_unit = COMPUTE_UNITS.GPU
         else:
             raise NotImplementedError(
-                f"Compute unit {self.compute_unit} was not implement for test {self.name}"
+                f"Compute unit {self.compute_unit} was not implemented for test {self.name}"
             )
 
     @run_after('init')
