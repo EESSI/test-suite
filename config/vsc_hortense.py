2bd58b883b4cb67b7922a7f6ffcad40e932ddec4
Lara Ramona Peeters <49882639+laraPPr@users.noreply.github.com>
2026-07-09 14:48:08 +0200

    remove unused os package

--- config/vsc_hortense.py
+++ config/vsc_hortense.py
@@ -20,8 +20,6 @@
 #    reframe --detect-host-topology \
 #        ~/.reframe/topology/hortense-{partition_name}/processor.json
 # ```
-import os
-
 from eessi.testsuite.common_config import (common_general_config, common_logging_config,
                                            get_sbatch_account, set_common_required_config)
 from eessi.testsuite.constants import EXTRAS, DEVICE_TYPES, FEATURES, GPU_VENDORS, SCALES
